0d6984ac09c80c96900451373ed66b3d2d2ef43a
dutianyu1234 <1615171019@qq.com>
2021-04-26 22:19:37 +0800

    small changes

---
+++ zhu.js
@@ -0,0 +1,9 @@
+function test(){
+  var tmp = document.getElementsByClassName("center")[0].children[0];
+  tmp.src = "./pic/zhu.png";
+}
+
+function test2(){
+  var tmp = document.getElementsByClassName("center")[0].children[0];
+  tmp.src = "./pic/zhu2.png";
+}
