20f4c0db079ba4d228e5f6809d28386871f893a5
dutianyu1234 <1615171019@qq.com>
2021-05-07 22:15:37 +0800

    1

--- IntroPages/PageSwitch.js
+++ IntroPages/PageSwitch.js
@@ -1,19 +1,24 @@
 var nowNum = 1;
 function LeftClick(name){
   var tmp = document.getElementsByClassName("center")[0].children[0];
-
   if (nowNum > 1){
     nowNum -= 1;
     tmp.src = "../pic/" + name + "/" + name + " (" + nowNum + ").png";
   }
-
-
 }
 
 function RightClick(name, maxNum){
   var tmp = document.getElementsByClassName("center")[0].children[0];
   if (nowNum < maxNum){
     nowNum += 1;
+    tmp.src = "../pic/" + name + "/" + name + " (" + nowNum + ").png";
+  }
+}
+
+function Menu(name, num){
+  if (name == "zhu"){
+    var tmp = document.getElementsByClassName("center")[0].children[0];
+    nowNum = num;
     tmp.src = "../pic/" + name + "/" + name + " (" + nowNum + ").png";
   }
 }
