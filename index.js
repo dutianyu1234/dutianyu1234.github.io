8553ffdf21b578c91bfe1f62eb4fbd4d4d9e4edb
dutianyu1234 <1615171019@qq.com>
2021-05-13 21:48:09 +0800

    1

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ document.onkeydown=function(event){
    if(e && e.keyCode==32){ 
      // backspace 键要做的事情
      var a=document.getElementsByClassName("q")[0];
-     a.innerHTML = "333";
+     //a.innerHTML = "333";
   }
 }
 
@@ -74,32 +74,4 @@ function show(e,name)
   else{
     pic.style.top = y-document.body.clientHeight*0.1 +'px';
   }
-  document.getElementsByClassName("q")[0].innerHTML = y + ' ' + document.body.clientHeight;
 };
-/*windowAddMouseWheel();
-function windowAddMouseWheel() {
-    var scrollFunc = function (e) {
-        e = e || window.event;
-        if (e.wheelDelta) {  //判断浏览器IE，谷歌滑轮事件
-            if (e.wheelDelta > 0) { //当滑轮向上滚动时
-                alert("滑轮向上滚动");
-            }
-            if (e.wheelDelta < 0) { //当滑轮向下滚动时
-                alert("滑轮向下滚动");
-            }
-        } else if (e.detail) {  //Firefox滑轮事件
-            if (e.detail> 0) { //当滑轮向上滚动时
-                alert("滑轮向上滚动");
-            }
-            if (e.detail< 0) { //当滑轮向下滚动时
-                alert("滑轮向下滚动");
-            }
-        }
-    };
-    //给页面绑定滑轮滚动事件
-    if (document.addEventListener) {
-        document.addEventListener('DOMMouseScroll', scrollFunc, false);
-    }
-//滚动滑轮触发scrollFunc方法
-    window.onmousewheel = document.onmousewheel = scrollFunc;
-}*/ 
