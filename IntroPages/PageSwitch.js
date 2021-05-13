3ec9863a7d68d326742cff1ba992d362df07e5d3
dutianyu1234 <1615171019@qq.com>
2021-05-14 00:15:50 +0800

    1

--- IntroPages/PageSwitch.js
+++ IntroPages/PageSwitch.js
@@ -1,22 +1,3 @@
-var images = new Array();
-function preload() {
-  var strUrl=window.location.href;
-  var arrUrl=strUrl.split("/");
-  var strPage=arrUrl[arrUrl.length-1];
-  if(strPage.indexOf("?")>-1){
-      var pageName=strPage.split("?");
-      strPage=pageName[0];
-  }
-  strPage = strPage.split(".")[0];
-
-    for (i = 0; i < 19; i++) {
-        images[i] = new Image();
-        images[i].src = "../pic/" + strPage + "/" + strPage + "(" + (i+1) + ").png";
-    }
-}
-preload();
-
-
 var nowNum = 1;
 function LeftClick(name){
   var tmp = document.getElementsByClassName("center")[0].children[0];
@@ -43,8 +24,35 @@ function Menu(name, num){
 }
 
 function showPage(){
-  var p = document.getElementsByClassName("page")[0];
-  p.innerHTML = "当前页数：" + nowNum;
+    var p = document.getElementsByClassName("page")[0];
+    p.innerHTML = "当前页数：" + nowNum;
 }
 
-setInterval("showPage()",100)
+var images = new Array();
+function preload() {
+  var strUrl=window.location.href;
+  var arrUrl=strUrl.split("/");
+  var strPage=arrUrl[arrUrl.length-1];
+  if(strPage.indexOf("?")>-1){
+      var pageName=strPage.split("?");
+      strPage=pageName[0];
+  }
+  strPage = strPage.split(".")[0]; 
+  
+  var p1 = document.getElementsByClassName("load")[0];
+
+  for (i = 0; i < 19; i++) {
+    p1.innerHTML = "正在加载图片：" + i;
+      images[i] = new Image();
+      images[i].src = "../pic/" + strPage + "/" + strPage + "(" + (i+1) + ").png";
+  }
+  p1.innerHTML = "";
+}
+
+
+window.onload=function(){
+//setInterval("showPage()",100);
+showPage();
+
+preload();
+}
